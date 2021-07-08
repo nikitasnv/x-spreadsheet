26bcdb7f5c09217ab6df3f517765a9f414752ac8
nikitasnv <nikitasnv@mail.ru>
2021-07-08 16:07:01 +0300

    -

---
+++ src/locale/ru.js
@@ -0,0 +1,12 @@
+export default {
+  formula: {
+    sum: 'СУММ',
+    average: 'СРЗНАЧ',
+    max: 'МАКС',
+    min: 'МИН',
+    _if: 'ЕСЛИ',
+    and: 'И',
+    or: 'ИЛИ',
+    concat: 'СЦЕПИТЬ',
+  }
+};
